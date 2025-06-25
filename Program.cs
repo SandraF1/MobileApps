a8baaf8d0a1aadd290e1c94c520e6128d0f18ae4
Sandra F <seeksandra@gmail.com>
2025-06-25 11:26:34 +1000

    3002

--- Program.cs
+++ Program.cs
@@ -90,7 +90,7 @@ builder.Services.AddCors(options =>
  
 builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    serverOptions.ListenAnyIP(3001); // HTTP
+    serverOptions.ListenAnyIP(3002); // HTTP
     // Optional: Add HTTPS with cert
     // serverOptions.ListenAnyIP(7100, listenOptions => listenOptions.UseHttps());
 });
